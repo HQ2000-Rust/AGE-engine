cca352bde019f46bf374ccd971d991611ac85674
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-26 23:22:02 +0200

    Update scheduler.rs

--- src/general/scheduler.rs
+++ src/general/scheduler.rs
@@ -1,23 +1,50 @@
-use wgpu::naga::FastHashMap;
-use crate::general::data::{DataTrait, GameData};
+use std::collections::{HashMap, VecDeque};
+use std::ops::Deref;
+use crate::general::data::GameData;
+use crate::general::events::EventHandle;
+use crate::general::aliases::*;
+
 
 pub(crate) struct Scheduler {
-    once: Vec<Box<dyn FnOnce(&mut FastHashMap<&'static str, GameData>)>>,
-    update: Vec<Box<dyn FnMut(&mut FastHashMap<&'static str, GameData>, EventHandle>)>>,
+    once: VecDeque<SetupFn>,
+    update: VecDeque<UpdateFn>,
+}
+
+impl Default for Scheduler {
+    fn default() -> Self {
+        Self {
+            once: VecDeque::new(),
+            update: VecDeque::new(),
+        }
+    }
 }
 
 impl Scheduler {
-    fn new(once: Vec<Box<dyn FnOnce(&mut FastHashMap<&'static str, GameData>)>>,
-           update: Vec<Box<dyn FnMut(&mut FastHashMap<&'static str, GameData>)>>) -> Self {
+    pub(crate) fn new(once: VecDeque<SetupFn>,
+           update: VecDeque<UpdateFn>) -> Self {
         Self {
             once,
             update,
         }
     }
-    fn setup(&self, resources: &mut FastHashMap<&'static str, GameData>) {
-        self.once.iter().for_each(|f| f(resources));
+    pub(crate) fn new_empty() -> Self {
+        Default::default()
     }
-    fn update(&self, resources: &mut FastHashMap<&'static str, GameData>) {
-        self.update.iter().for_each(|f| f(resources));
+    pub(crate) fn add_once(&mut self,once: impl FnMut(&mut Resources, &mut EventHandle) + 'static) {
+        self.once.push_back(Box::new(once));
+    }
+    pub(crate) fn add_update(&mut self,once: impl FnMut(&mut Resources, &mut EventHandle) + 'static) {
+        self.update.push_back(Box::new(once));
+    }
+    pub(crate) fn setup(&mut self, resources: &mut HashMap<&'static str, GameData>, event_handle: &mut EventHandle ) {
+        //I hope I can remove that .collect() later...just have no idea how
+        for f in self.once.iter_mut() {
+            f(resources, event_handle);
+        }
+    }
+    pub(crate) fn update(&mut self, resources: &mut HashMap<&'static str, GameData>, event_handle: &mut EventHandle) {
+        for f in self.update.iter_mut() {
+            f(resources, event_handle);
+        }
     }
 }
