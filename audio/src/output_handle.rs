ad7efcd8f5308c3f4015ef79139df2b202aab6d0
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-08-09 23:40:15 +0200

    Update output_handle.rs

--- audio/src/output_handle.rs
+++ audio/src/output_handle.rs
@@ -1,11 +1,7 @@
 use super::errors::AudioError;
-use super::output::*;
 use super::output_handle::output_markers::{OutputDisabled, OutputEnabled};
 use super::traits::marker::OutputHandlerState;
-use cpal::Stream;
-use cpal::traits::{DeviceTrait, HostTrait};
-use rodio::{Decoder, OutputStream, OutputStreamHandle, Sink, source::Source};
-use std::any::type_name_of_val;
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Cursor, Read};
@@ -13,23 +9,16 @@ use std::marker::PhantomData;
 use std::path::Path;
 
 pub mod output_markers {
-    use super::input_markers::InputDisabled;
-
     pub struct OutputDisabled;
-    //pub(super) type OutDis=OutputDisabled;
     pub struct OutputEnabled;
-    //pub(super) type OutEn=OutputEnabled;
 }
 pub mod input_markers {
     pub struct InputEnabled;
-    //pub(super) type InEn=InputEnabled;
     pub struct InputDisabled;
-    //pub(super) type InDis=InputDisabled;
 }
 
 pub struct OutputHandle<O> {
     pub(super) stream: Option<OutputStream>,
-    pub(super) stream_handle: Option<OutputStreamHandle>,
     pub(super) sink: Option<Sink>,
     pub(super) loaded_files: HashMap<String, Cursor<Vec<u8>>>,
     _marker: PhantomData<O>,
@@ -39,34 +28,10 @@ impl Default for OutputHandle<OutputDisabled> {
     fn default() -> OutputHandle<OutputDisabled> {
         OutputHandle {
             stream: None,
-            stream_handle: None,
             sink: None,
             loaded_files: HashMap::new(),
             _marker: PhantomData,
         }
-    }
-}
-
-impl<O: OutputHandlerState> std::fmt::Debug for OutputHandle<O> {
-    //tmp
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "\n{}:\nHandle_is_none: {}\n- loaded files: {:?}\n",
-            {
-                match type_name_of_val(self) {
-                    "age_engine::audio::audio_handles::OutputHandle<age_engine::audio::audio_handles::output_markers::OutputEnabled>" => {
-                        "OutputHandle<OutputEnabled>"
-                    }
-                    "age_engine::audio::audio_handles::OutputHandle<age_engine::audio::audio_handles::output_markers::OutputDisabled>" => {
-                        "OutputHandle<OutputDisabled>"
-                    }
-                    _ => unreachable!(),
-                }
-            },
-            self.stream_handle.is_none(),
-            self.loaded_files.keys().collect::<Vec<&String>>(),
-        )
     }
 }
 
@@ -79,8 +44,8 @@ impl<O: OutputHandlerState> OutputHandle<O> {
     ) -> Result<(), AudioError> {
         //capacity: 50 Kilobytes, every sound effect has at least this size...
         let mut buffer = Vec::with_capacity(1024 * 50);
-        let mut file = File::open(path)?;
-        file.read_to_end(&mut buffer)?;
+        let mut file = File::open(path).map_err(AudioError::IoError)?;
+        file.read_to_end(&mut buffer).map_err(AudioError::IoError)?;
         let data = Cursor::new(buffer);
         self.loaded_files.insert(new_name, data);
         Ok(())
@@ -107,21 +72,18 @@ impl OutputHandle<OutputDisabled> {
 
     //maybe one day I'll find a shorter name...
     pub fn activate_output(self) -> Result<OutputHandle<OutputEnabled>, AudioError> {
-        let (stream, stream_handle) = OutputStream::try_default()?;
+        let builder = OutputStreamBuilder::from_default_device().map_err(AudioError::OutputStreamBuilderError)?;
+        let stream = builder.open_stream().map_err(AudioError::StreamError)?;
         let mut result = OutputHandle::<OutputEnabled> {
             stream: Some(stream),
-            stream_handle: Some(stream_handle),
             sink: None,
             loaded_files: self.loaded_files,
             _marker: PhantomData,
         };
-        result.sink = match result.stream_handle {
-            //match because otherwise I can't destructure the Option without taking ownership of the stream_handle
-            Some(ref stream_handle) => Some(Sink::try_new(&stream_handle)?),
-            None => {
-                unreachable!();
-            }
-        };
+        let stream_ref=result.stream.as_ref().expect("Just set it to Some(_)");
+        let mixer=stream_ref.mixer();
+
+        result.sink = Some(Sink::connect_new(mixer));
         Ok(result)
     }
 }
@@ -133,7 +95,6 @@ impl OutputHandle<OutputEnabled> {
     fn disable_output(self) -> OutputHandle<OutputDisabled> {
         OutputHandle::<OutputDisabled> {
             stream: None,
-            stream_handle: None,
             sink: None,
             loaded_files: self.loaded_files,
             _marker: PhantomData,
@@ -141,8 +102,8 @@ impl OutputHandle<OutputEnabled> {
     }
 
     pub fn play_from_file<P: AsRef<Path>>(&mut self, path: P) -> Result<(), AudioError> {
-        let reader = BufReader::new(File::open(path)?);
-        let decoder = Decoder::new(reader)?;
+        let reader = BufReader::new(File::open(path).map_err(AudioError::IoError)?);
+        let decoder = Decoder::new(reader).map_err(AudioError::DecoderError)?;
         self.sink
             .as_mut()
             .expect("Not 'None' if the output is activated")
@@ -155,28 +116,15 @@ impl OutputHandle<OutputEnabled> {
             Some(data) => data.clone(),
             None => return Err(AudioError::FileNotLoaded),
         };
-        let decoder = Decoder::new(data)?;
+        let decoder = Decoder::new(data).map_err(AudioError::DecoderError)?;
         self.sink
             .as_mut()
             .expect("Basically never 'None' if output is enabled")
             .append(decoder);
-        //dbg artifacts
-        /*handle.append(decoder);
-        handle.set_volume(10.0);
-        dbg!(handle.len());
-        dbg!(handle.volume());
-        dbg!(handle.speed());
-        dbg!(handle.is_paused());
-        handle.sleep_until_end();
-        dbg!("end");
-        loop {
-            std::thread::sleep(std::time::Duration::from_secs(1));
-            dbg!(handle.get_pos());
-        }*/
         Ok(())
     }
 
-    fn pause(&mut self /*, id: u32*/) {
+    fn pause(&mut self) {
         self.sink
             .as_mut()
             .expect("Basically never 'None' if output is enabled")
@@ -197,9 +145,4 @@ impl OutputHandle<OutputEnabled> {
             None => unreachable!("Basically never 'None' if output is enabled"),
         }
     }
-
-    //other play functions...
-    //channels?
-    //ids?
-    //(no)
 }
