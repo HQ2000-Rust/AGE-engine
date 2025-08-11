132bcaa6461e92d451a46d5b851d0d6b2be73a3a
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-08-11 13:48:12 +0200

    Update traits.rs

--- audio/src/traits.rs
+++ audio/src/traits.rs
@@ -1,13 +1,8 @@
-use super::output_handle::{input_markers, output_markers};
-
 //maybe I will remove them idk
 pub mod marker {
-    use crate::output_handle::{input_markers, output_markers};
+    use crate::output_handle::{output_markers};
 
     pub trait OutputHandlerState {}
     impl OutputHandlerState for output_markers::OutputDisabled {}
     impl OutputHandlerState for output_markers::OutputEnabled {}
-    pub trait InputHandlerState {}
-    impl InputHandlerState for input_markers::InputDisabled {}
-    impl InputHandlerState for input_markers::InputEnabled {}
 }
