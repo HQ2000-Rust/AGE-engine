70d69f13edd670dacd956e1d81538c6b6741dd0c
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-09-21 13:17:47 +0200

    Create mod.rs

---
+++ src/commands/camera/mod.rs
@@ -0,0 +1,16 @@
+mod impls;
+
+pub struct SetSpeed(pub f32);
+pub struct Up;
+pub struct UpWithSpeed(pub f32);
+
+pub struct Down;
+pub struct DownWithSpeed(pub f32);
+pub struct Left;
+pub struct LeftWithSpeed(pub f32);
+pub struct Right;
+pub struct RightWithSpeed(pub f32);
+pub struct Forward;
+pub struct ForwardWithSpeed(pub f32);
+pub struct Backward;
+pub struct BackwardWithSpeed(pub f32);
