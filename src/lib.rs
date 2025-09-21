f93705faf1a9a5cbd3cb1d3f94f8cdc1ecbe5926
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-09-21 13:15:00 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-pub mod aliases;
+mod aliases;
+mod camera;
 mod commands;
 mod custom_events;
 mod events;
-pub mod game;
+mod game;
 mod game_fn;
 mod info;
 pub mod prelude;
 mod scheduler;
+mod world;
