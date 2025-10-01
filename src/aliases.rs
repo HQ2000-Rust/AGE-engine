2b6406f80cdbf69d5c9bb78a35467def341197da
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-10-01 06:42:29 +0200

    Update aliases.rs

--- src/aliases.rs
+++ src/aliases.rs
@@ -1,8 +1,10 @@
 use crate::commands::CommandHandle;
 use crate::events::EventHandle;
-use anymap::AnyMap;
+use anymap::{AnyMap, Map};
 pub(crate) type BoxedGameFn =
-    Box<dyn FnMut(&mut Commands, &mut AnyMap, &mut EventHandle) + 'static>;
+    Box<dyn FnMut(&mut Commands, &mut SendAnyMap, &mut EventHandle) + 'static + Send>;
 pub(crate) type _MaxParamGameFn = Box<dyn FnMut(&mut Commands, &mut AnyMap, &mut Events) + 'static>;
 pub type Commands = CommandHandle;
 pub type Events = EventHandle;
+
+pub(crate) type SendAnyMap = Map<dyn anymap::any::Any + Send>;
