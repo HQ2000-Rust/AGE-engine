ee13fc0036f414bb94a3ef78760c0a48cf31d17c
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2025-12-31 14:05:44 +0100

    2nd

---
+++ crates/age-audio/src/lib.rs
@@ -0,0 +1,6 @@
+//TODO: make audio ECS-ready
+
+//maybe not everything needs to be pub in the end, idk
+pub mod errors;
+pub mod output_handle;
+pub mod traits;
---
+++ crates/age-audio/src/traits.rs
@@ -0,0 +1,8 @@
+//maybe I will remove them idk
+pub mod marker {
+    use crate::output_handle::output_markers;
+
+    pub trait OutputHandlerState {}
+    impl OutputHandlerState for output_markers::OutputDisabled {}
+    impl OutputHandlerState for output_markers::OutputEnabled {}
+}
